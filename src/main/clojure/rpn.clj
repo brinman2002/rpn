ee256e040e792576424a2d0fddccf60ba24eeb3c
Brandon Inman <brandonathome@outlook.com>
2015-06-15 23:00:22 -0500

    Can sort of tokenize a number. primary challenge at the moment is dealing with the end of the stream.

--- src/main/clojure/rpn.clj
+++ src/main/clojure/rpn.clj
@@ -1,18 +1,31 @@
 (ns rpn
    (:gen-class :methods [#^{:static true} [compileString [String] void]])
-   ;(:gen-class :methods [#^{:static true} [doit [InputStream] void]])
 )
 
+(defn ^:private tokenizeDigit [c is tok]
+  (cond
+    (some #{c} (map char (range (int \0) (int \9)))) (recur (char (.read is)) is (str tok c))
+    :else {"c" c "token" tok }))
 
-(defn -lex [is] 
+(defn ^:private tokenize [c is]
+  (cond
+    ; Digits 0..9
+    (some #{c} (map char (range (int \0) (int \9)))) (println (tokenizeDigit c is ""))
+    ; Alpha characters a..z
+    (some #{c} (map char (range (int \a) (int \z)))) (println "char: " c) 
+    (some #{c} #{\ }) (println "<space>")
+    :else (throw (Exception. (.concat "Unknown token in stream " (.toString c))))))
+
+
+(defn ^:private lex [is] 
   (let [byteRead (.read is) ]
     (cond 
       (< byteRead 0) '() 
-      :else ( do (println (char byteRead)) (recur is))
-    )
+      :else ( do (tokenize (char byteRead) is) (recur is))))) 
+
+
+(defn ^:private parse [tokens]
   )
-)
 
 (defn -compileString [theString]
-  (-lex (new java.io.ByteArrayInputStream (. theString getBytes)))
-)
+  (lex (new java.io.ByteArrayInputStream (. theString getBytes))))
