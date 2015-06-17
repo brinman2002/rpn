687b1e1723eb4ac0efff719ccd2d16379dc1388f
Brandon Inman <brandonathome@outlook.com>
2015-06-16 22:46:19 -0500

    minor refactor

--- src/main/clojure/rpn.clj
+++ src/main/clojure/rpn.clj
@@ -2,17 +2,27 @@
    (:gen-class :methods [#^{:static true} [compileString [String] void]])
 )
 
+(def DIGIT_PATTERN (re-pattern "^[0-9]+$"))
+(def SYMBOL_PATTERN (re-pattern "^[a-zA-Z]+"))
+
 (defn ^:private tokenizeDigit [c is tok]
   (cond
-    (some #{c} (map char (range (int \0) (int \9)))) (recur (char (.read is)) is (str tok c))
+    (some #{c} (re-matches DIGIT_PATTERN (str c))) (recur (char (.read is)) is (str tok c))
     :else {"c" c "token" tok }))
 
+;TODO see if we can pass in a matcher instead of one method per concern
+(defn ^:private tokenizeSymbol [c is tok]
+  (cond
+    (some #{c} (re-matches SYMBOL_PATTERN (str c))) (recur (char (.read is)) is (str tok c))
+    :else {"c" c "token" tok }))
+
+;TODO broken, needs to deal with returned 'c' value as well as collecting tokens
 (defn ^:private tokenize [c is]
   (cond
     ; Digits 0..9
-    (some #{c} (map char (range (int \0) (int \9)))) (println (tokenizeDigit c is ""))
+    (some #{c} (re-matches DIGIT_PATTERN (str c))) (println (tokenizeDigit c is ""))
     ; Alpha characters a..z
-    (some #{c} (map char (range (int \a) (int \z)))) (println "char: " c) 
+    (some #{c} (re-matches SYMBOL_PATTERN (str c))) (println (tokenizeSymbol c is ""))
     (some #{c} #{\ }) (println "<space>")
     :else (throw (Exception. (.concat "Unknown token in stream " (.toString c))))))
 
